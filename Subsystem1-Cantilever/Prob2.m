552e29b7f5d5be9e847c2841a5acd01a6b6e8633
weselyn1995 <33514540+weselyn1995@users.noreply.github.com>
2018-12-05 19:45:37 +0000

    Adding frequency coefs and changing nlcon to take parameters from the main script.

--- Subsystem1-Cantilever/Prob2.m
+++ Subsystem1-Cantilever/Prob2.m
@@ -1,4 +1,19 @@
+
+%% parameters
+Fl = 7.84;
+mm = 0.06;
+md = 1.4;
+mr = 0.008;
+g = 9.8;
+E = 164000000000;
 ro = 1780;
+k = 1.875;
+k2 = 4.694;
+k3 = 7.855;
+
+sigmax = 2780000000;
+defmax = 0.001;
+fmin = 250;
 
 objective = @(x) (x(1)*x(2)-((x(1)-2*x(3))*(x(2)-2*x(3))))*x(4)*ro;
 
@@ -18,8 +33,9 @@ b = [];
 Aeq = [];
 beq = [];
 
+Fl =7.84;
 % nonlinear constraints
-nonlincon = @nlcon;
+nonlincon = @(x)nlcon1(x, Fl, mm, g, md, E, ro, k, sigmax, defmax, fmin);
 
 % optimize with fmincon
 %[X,FVAL,EXITFLAG,OUTPUT,LAMBDA,GRAD,HESSIAN] 
@@ -29,6 +45,7 @@ nonlincon = @nlcon;
 % show final objective
 disp(['Final arm weight [kg]: ' num2str(objective(x))])
 
+[c, ceq] = nlcon1(x, Fl, mm, g, md, E, ro, k, sigmax, defmax, fmin)
 % print solution
 disp('Solution')
 disp(['ef = ' num2str(ef)])
@@ -39,6 +56,7 @@ disp(['x4 (L) [m] = ' num2str(x(4))])
 disp(['sigx [Pa] = ' num2str(((343*x(4)*x(2))/(25*((x(1)*x(2)^3)/6 - ((x(1) - 2*x(3))*(x(2) - 2*x(3))^3)/6))))])
 disp(['sigy [Pa] = ' num2str(((343*x(4)*x(1))/(25*((x(1)^3*x(2))/6 - ((x(1) - 2*x(3))^3*(x(2) - 2*x(3)))/6))))])
 disp(['defx [m] = ' num2str(((196*x(4)^3)/(25*(41000000000*x(1)*x(2)^3 - 41000000000*(x(1) - 2*x(3))*(x(2) - 2*x(3))^3))))])
+disp(['f1nf [Hz] = ' num2str(c(4))])
 disp(['f1 [Hz] = ' num2str(((140625*2^(1/2)*((12500000*x(1)^3*x(2) - 12500000*(x(1) - 2*x(3))^3*(x(2) - 2*x(3)))/(12500000*(1780*x(1)*x(2) - 1780*(x(1) - 2*x(3))*(x(2) - 2*x(3)))))^(1/2))/16))])
 disp(['f2 [Hz] = ' num2str((6201917179951071*(((41000000000*x(1)^3*x(2))/3 - (41000000000*(x(1) - 2*x(3))^3*(x(2) - 2*x(3)))/3)/(1780*x(1)*x(2) - 1780*(x(1) - 2*x(3))*(x(2) - 2*x(3))))^(1/2))/281474976710656)])
 disp(['f3 [Hz] = ' num2str((8683647287449303*(((41000000000*x(1)^3*x(2))/3 - (41000000000*(x(1) - 2*x(3))^3*(x(2) - 2*x(3)))/3)/(1780*x(1)*x(2) - 1780*(x(1) - 2*x(3))*(x(2) - 2*x(3))))^(1/2))/140737488355328)])
