5f0bb34a5c69408824c1f59b5dc36e512e1b31f1
weselyn1995 <33514540+weselyn1995@users.noreply.github.com>
2018-12-05 21:30:25 +0000

    Enabling the script to be used as a function as part of a larger system.

---
+++ Subsystem1-Cantilever/TestSL.m
@@ -0,0 +1,9 @@
+clear all
+
+Fl = 7.84;
+mm = 0.06;
+md = 1.4;
+rpm = 14000;
+r = 0.1867;
+
+[x, w] = Cantilever(Fl,mm,md,rpm,r);
--- Subsystem1-Cantilever/nlcon1.m
+++ Subsystem1-Cantilever/nlcon1.m
@@ -11,15 +11,15 @@ Mm = Fm * x(4);
 Fd = md * g;
 Md = Fd * x(4);
 
-sigxl = Ml*x(2)/(2*Ix)
-sigxd = Md*x(2)/(2*Ix)
-sigxm = Mm*x(2)/(2*Ix)
-sigyd = Md*x(1)/(2*Iy)
+sigxl = Ml*x(2)/(2*Ix);
+sigxd = Md*x(2)/(2*Ix);
+sigxm = Mm*x(2)/(2*Ix);
+sigyd = Md*x(1)/(2*Iy);
 
-delxl = Fl*x(4)^3/(3*E*Ix)
+delxl = Fl*x(4)^3/(3*E*Ix);
 
-fnf1 = k^2*sqrt(E*Iy/(ro*(x(1)*x(2)-(x(1)-2*x(3))*(x(2)-2*x(3)))))
-fnf2 = k^2*sqrt(E*Ix/(ro*(x(1)*x(2)-(x(1)-2*x(3))*(x(2)-2*x(3)))))
+fnf1 = k^2*sqrt(E*Iy/(ro*(x(1)*x(2)-(x(1)-2*x(3))*(x(2)-2*x(3)))));
+fnf2 = k^2*sqrt(E*Ix/(ro*(x(1)*x(2)-(x(1)-2*x(3))*(x(2)-2*x(3)))));
 
 c1 = -sigmax+sigxd;
 c2 = -sigmax+sigyd;
