9b59bafbc15e8071d0e1f328f6df470e46c63064
Scott-Bunting <scott.bunting15@imperial.ac.uk>
2018-12-08 18:20:54 +0000

    Tidying and commenting code

--- Subsystem2-Rotors/constraintFunction.m
+++ Subsystem2-Rotors/constraintFunction.m
@@ -1,7 +1,13 @@
 function [c,ceq] = constraintFunction(x, rho, sig)
 
+    %Input variables
+    %{
+    x = Design Variables
+    rho = Density of Material
+    sig = Yield Stress of Material
+    %}
+
     %Parameters for non-linear constraint
-    
     n_r = 4; %No. of rotors
     n_b = 2; %No. of blades per rotor
     m_d = 1.4; %Mass of drone
@@ -12,15 +18,23 @@ function [c,ceq] = constraintFunction(x, rho, sig)
     Pf = 2; %Power factor
     FOS = 1.5; %Safety factor
     
-    %Non-linear Thrust Equation
+    %% Non-linear Constraints (Inequalities)
+    
+    %Thrust
     c1 = (Pf*m_d*g)/n_r - 2*n_r*n_b*(x(3)^2 - x(4)^2)*x(1)*sin(theta)*omega*rho_air*g...
         + pi*rho*x(1)*n_r*n_b*(x(2)*(x(3) - x(4)) + x(5)*x(4));
     
-    %sig is the yield stress of the material
+    %Root Stress
     c2 = (FOS*m_d*g*(x(4)))/(n_r*n_b*pi*x(5)*x(2)^3) - sig;
+    
+    %Concatenating the Constraints
     c = [c1;
         c2];
-    %Nonlinear Stress constraints
+    
+    %Deflection - to be completed
+    
+    %% Non-linear Constraints (Equalities)
+    % Currently none - need to check activity
     ceq = 0;
     
     
