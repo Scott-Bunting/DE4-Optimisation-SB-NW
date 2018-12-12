37513d477c1e6b35f160f0b2b4d04299b2b0a7fe
Scott-Bunting <scott.bunting15@imperial.ac.uk>
2018-12-12 18:38:00 +0000

    Correcting non-linear constraints

--- Subsystem2-Rotors/constraintFunction.m
+++ Subsystem2-Rotors/constraintFunction.m
@@ -21,17 +21,16 @@ function [c,ceq] = constraintFunction(x, rho, sig, E)
     %% Non-linear Constraints (Inequalities)
     
     %Thrust
-    c1 = (Pf*m_d*g)/n_r - 2*n_r*n_b*(x(3)^2 - x(4)^2)*x(1)*sin(theta)*omega*rho_air*g...
-        + pi*rho*x(1)*n_r*n_b*(x(2)*(x(3) - x(4)) + x(5)*x(4));
+    Thrust = (x(3)^2 - x(4)^2)*x(1)*sin(theta)*omega*rho_air*g;
+    c1 = (Pf*m_d*g)/n_r - 2*n_r*n_b*(Thrust + pi*rho*x(1)*n_r*n_b*(x(2)*(x(3) - x(4)) + x(5)*x(4)));
     
     %Root Stress
-    c2 = (FOS*m_d*g*(x(4)))/(n_r*n_b*pi*x(5)*x(2)^3) - sig;
+    c2 = sig - (FOS*m_d*g*(x(4)/2))/(2*n_r*n_b*pi*x(5)*x(2)^3);
     
     %Deflection
-    Thrust = (x(3)^2 - x(4)^2)*x(1)*sin(theta)*omega*rho_air*g;
     I = (pi/4)*(x(1)/2)*(x(2)/2)^3;
-    omega = Thrust/x(4);
-    c3 = (omega*x(4)^4/8*E*I) - 1*10^-10;
+    omega = Thrust/x(3);
+    c3 = (omega*x(3)^4/8*E*I) - 0.001;
     
     %Concatenating the Constraints
     c = [c1;
